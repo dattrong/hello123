81af20a1198141f43a78fd2011bd3728b2cca3a0
dattrong <52957785+dattrong@users.noreply.github.com>
2019-09-02 08:57:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,51 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html>
-<title>BABY LADY</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>BABY LADY</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<body style="background: palegoldenrod" style="max-width: 100%" >
+    <body style="background: pink" style="max-width: 100%" >
 
-<div style="margin-left:10%">
-...Welcome to Baby's world...
-</div>
-
-  <div class="w3-main" style="margin-left:300px">
-
-  <!-- Header -->
-  <header>
-     
-    
-        <div class="w3-container ">
-            <center><img src="images/1.jpg" style="margin-right:30%" style="max-width: 100%" ></center>
-                <!-- Sidebar/menu -->
-                <div class="w3-bar w3-papayawhip" style="padding-left: 20%">
-        <a href="#Princess" class="w3-bar-item w3-button w3-mobile">Prinsess</a>
-        <a href="#BlackBaby" class="w3-bar-item w3-button w3-mobile">BlackBaby</a>
-        <a href="#Mermaid" class="w3-bar-item w3-button w3-mobile">Mermaid</a>
-        <a href="admin.php"class="w3-bar-item w3-button w3-mobile" style="margin-left: 500px">Admin</a>
-
+        <div style="margin-left:10%">
+            ...Welcome to Baby's world...
         </div>
-        <div class="list w3-row" style="color: pink">
-            <div class="" id="Princess" style="width: 500px"><h2>Princess</h2>
-                <img class="mySlides" src="images/8.jpg" style="width:100%">
-            </div>        
-                    <div class="list w3-row" style="color: pink">
-                    <div class=""id="BlackBaby" style="width: 500px"><h2>BlackBaby</h2>
-                <img class="mySlides" src="images/7.jpg" style="width:100%">
-            <div class="list w3-row" style="color: pink">
-                    <div class=""id="Mermaid" style="width: 500px"><h2>Mermaid</h2>
-                <img class="mySlides" src="images/3.jpg" style="width:100%">
-            
 
-            </div>
-        </div>
-  </header>
-        
-        
-  
-  </footer>
-  
-  <div class="w3-pink w3-center w3-padding-24">LOVE YOU</div>
+        <div class="w3-main" style="margin-left:300px">
+            <header>
 
-</div> 
+
+                <div class="w3-container ">
+                    <center><img src="images/1.jpg" style="margin-right:30%" style="max-width: 100%" ></center>
+                    <div class="w3-bar w3-papayawhip" style="padding-left: 20%">
+                        <a href="#Princess" class="w3-bar-item w3-button w3-mobile">Prinsess</a>
+                        <a href="#BlackBaby" class="w3-bar-item w3-button w3-mobile">BlackBaby</a>
+                        <a href="#Mermaid" class="w3-bar-item w3-button w3-mobile">Mermaid</a>
+                        <a href="admin.php"class="w3-bar-item w3-button w3-mobile" style="margin-left: 500px">Admin</a>
+
+                    </div>
+                    <div class="list w3-row" style="color: plum">
+                        <div class="" id="Princess" style="width: 500px"><h2>Princess</h2>
+                            <img class="mySlides" src="images/8.jpg" style="width:100%">
+                        </div>        
+                        <div class="list w3-row" style="color: plum">
+                            <div class=""id="BlackBaby" style="width: 500px"><h2>BlackBaby</h2>
+                                <img class="mySlides" src="images/7.jpg" style="width:100%">
+                                <div class="list w3-row" style="color: plum">
+                                    <div class=""id="Mermaid" style="width: 500px"><h2>Mermaid</h2>
+                                        <img class="mySlides" src="images/3.jpg" style="width:100%">
+
+
+                                    </div>
+                                </div>
+                                </header>
+
+                                <div class="w3-pink w3-center w3-padding-24">LOVE YOU</div>
+
+                            </div> 
+                        </body>
+                        </html>
